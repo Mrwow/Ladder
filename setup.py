45340dbdd9a1698bdba0f186037b1e297333fa36
Zhou T <14700737+Mrwow@users.noreply.github.com>
2023-08-28 14:01:18 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -19,13 +19,12 @@ setuptools.setup(
     license='GPLv3',
     packages=['ladder'],
     # include_package_data=True,
-    install_requires=['numpy', 'pandas','tqdm',
-                      # data processing
-                      'qtpy!=1.11.2', 'PyQt5!=5.15.3, !=5.15.4', 'pyside2',
+    install_requires=['numpy>=1.18.5', 'pandas>=1.1.4', 'scipy>=1.4.1', 'matplotlib>=3.2.2', 'seaborn>=0.11.0', 'Pillow>=7.1.2', 'opencv-python>=4.1.1', 
+                      # data processing, image processing
+                      'qtpy!=1.11.2', 'PyQt5!=5.15.3, !=5.15.4', 'pyside2', 'tqdm>=4.64.0', 'imgviz>=0.11', 'requests>=2.23.0','PyYAML>=5.3.1',
                       # GUI
-                      'pillow', 'opencv-python', 'matplotlib', 'imgviz>=0.11', 'seaborn',
-                      # image processing
-                      'torch==1.9.0', 'tensorboard', 'torchvision==0.10.0'
+                      'torch>=1.7.0', 'torchvision>=0.8.1', 'tensorboard>=2.4.1'
+                      # deep learning
                       ],
     entry_points={
                  "console_scripts": ["ladder=ladder.__main__:main"],
