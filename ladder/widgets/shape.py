7656f281e87056d67ff281febb9e96b0dca2c8be
Zhou T <14700737+Mrwow@users.noreply.github.com>
2023-11-30 14:38:34 -0800

    Update shape.py
    make dot size smaller in the shape

--- ladder/widgets/shape.py
+++ ladder/widgets/shape.py
@@ -51,7 +51,7 @@ class Shape(object):
     vertex_fill_color = DEFAULT_VERTEX_FILL_COLOR
     hvertex_fill_color = DEFAULT_HVERTEX_FILL_COLOR
     point_type = P_ROUND
-    point_size = 8
+    point_size = 2
     scale = 1.0
     def __init__(
             self,
